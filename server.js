29a5f09be2c54e518ad887be52a524dca777cea0
Agus Bira <agusbira398@gmail.com>
2025-07-03 23:36:07 -0300

    fix: put

--- server.js
+++ server.js
@@ -28,6 +28,22 @@ app.post('/', (req, res) => {
 	productos.push(nuevoProducto)
 	res.status(200).json(nuevoProducto)
 });
+app.put('/:id', (req, res) => {
+	const {title, description, code, price, status, stock , category , thumbnails} = req.body
+	const {id} = req.params
+	productoFiltrado = productos.find(p => p.id === parseInt(id))
+	productoFiltrado.title = title ||  productoFiltrado.title
+	productoFiltrado.description = description ||  productoFiltrado.description
+	productoFiltrado.code = code ||  productoFiltrado.code
+	productoFiltrado.price = price ||  productoFiltrado.price
+	productoFiltrado.status = status ||  productoFiltrado.status
+	productoFiltrado.stock = stock ||  productoFiltrado.stock
+	productoFiltrado.category = category ||  productoFiltrado.category
+	productoFiltrado.thumbnails = thumbnails ||  productoFiltrado.thumbnails
+
+	res.json(productoFiltrado)
+	res.status(200).json(nuevoProducto)
+});
 
 app.listen(PORT, () => {
 	console.log("Servido escuchando en el puerto" + PORT)
